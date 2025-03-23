89aeb79eb6cf27f4eb81f052505f6e3878646f92
viniciusnorberto <vinicius.norberto@hubbrasil.com.br>
2025-03-23 13:44:50 -0300

    Initialize project authenticator and appointment book

---
+++ appointment-book/src/main/java/com/br/appointment_book/AppointmentBookApplication.java
@@ -0,0 +1,13 @@
+package com.br.appointment_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentBookApplication.class, args);
+	}
+
+}
---
+++ appointment-book/src/test/java/com/br/appointment_book/AppointmentBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.appointment_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
